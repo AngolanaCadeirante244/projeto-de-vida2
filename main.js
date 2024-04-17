8897b076bff710d2c8625040222d143f4ba3c2d7
Thiaguinho <132918384+AngolanaCadeirante244@users.noreply.github.com>
2024-04-17 13:46:25 -0300

    FINAL 1

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2028-09-T00:00:00");
+const tempoObjetivo1 = new Date("2028-12-10T00:00:00");
 const tempoObjetivo2 = new Date("2030-06-03T00:00:00");
 const tempoObjetivo3 = new Date("2025-06-03T00:00:00");
 const tempoObjetivo4 = new Date("2025-01-01T00:00:00");
